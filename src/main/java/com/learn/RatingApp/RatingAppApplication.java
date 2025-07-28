530828d2b0a0d22e39a2691d8d6dcd47073cc3ac
Deepak Yadav <deepakyadav1331@gmail.com>
2025-07-29 01:12:25 +0530

    Adding Eureka Client dependencies, enabled @EnableDiscoveryClient and added Eureka configs

--- src/main/java/com/learn/RatingApp/RatingAppApplication.java
+++ src/main/java/com/learn/RatingApp/RatingAppApplication.java
@@ -2,7 +2,9 @@ package com.learn.RatingApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class RatingAppApplication {
 
